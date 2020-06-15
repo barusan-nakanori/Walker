da4f44a1b966f8e6763c183af79af5ecf280f469
Norio Nakamura <nori.work.vtr250@gmail.com>
2020-06-15 18:53:48 +0000

    [fix]github

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
+    get 'logout', to: 'devise/sessions#destroy'
+  end
   root 'static_pages#home'
   get '/contact', to: 'static_pages#contact'
   resources :users, only: [:index,:show,:edit]
